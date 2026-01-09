df26da64b79d6ad6c4ded9526179ceadc279c9b6
Abdul Muqsit <92204326+amuqsit57@users.noreply.github.com>
2026-01-09 16:09:15 +0500

    Added basic header

--- my-app/components/layout/Header.tsx
+++ my-app/components/layout/Header.tsx
@@ -1,18 +1,39 @@
 import Image from "next/image";
+import { BiSearch } from "react-icons/bi";
+import { CiShoppingCart } from "react-icons/ci";
 
 export default function Header(){
     return(
         <>
         <header>
-            <div className="bg-primary  ">
+            <div className="flex items-center justify-between bg-secondary/30 p-2">
                 {/* Logo */}
-                <div className="div">
+                <div className="flex items-center justify-center gap-2">
                     <Image 
                     src='/assets/logo.png'
-                    width={20}
-                    height={20}
+                    width={24}
+                    height={24}
                     alt="Logo"
                     />
+                    <h1 className="text-foreground text-lg font-bold">Shopsy</h1>
+                </div>
+                {/* Options */}
+                <div className=" flex items-center justify-center gap-2 rounded-md">
+  
+                        <div className="flex items-center justify-center bg-background rounded-md p-[4px] ">
+                            <input type="text"  className=" outline-none" />
+                            <BiSearch 
+                            />
+                        </div>
+                        <div className="bg-primary p-2 rounded-full ">
+                              <CiShoppingCart 
+                            className="w-5 h-5 text-primaryForeground"
+                            />
+                      
+                        </div>
+
+                        
+                        
                 </div>
             </div>
         </header>
